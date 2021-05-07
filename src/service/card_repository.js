f265145fc1250508a19513ae13890b2573f7208a
youmeSon <rosydarling@naver.com>
2021-05-07 14:51:37 +0100

    fix the error of the 'firebase remove ( )'

--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -14,7 +14,7 @@ class CardRepository {
   }
 
   removeCard(userId, card) {
-    firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove(card); //사용자 id 안에 있는 cards의 안에 있는 id 저장~!
+    firebaseApp.database().ref(`${userId}/cards/${card.id}`).remove(); //사용자 id 안에 있는 cards의 안에 있는 id 저장~!
   }
 }
 
